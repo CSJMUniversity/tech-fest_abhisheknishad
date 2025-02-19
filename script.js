0b3855673b8f3a000c3decd94288155f53d55ce7
CSJM University <csjmuniversity6@gmail.com>
2025-02-19 13:08:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.from("#nav img",{
+    y:-40,
+    duration:1,
+    delay:1,
+    opacity:0,
+
+})
+gsap.from("#nav a",{
+    y:-40,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:.5
+
+})
+gsap.from("#main h1",{
+    x:50,
+    y:40,
+    duration:1,
+    delay:1.5,
+    opacity:0,
+    stagger:.5
+
+})
+gsap.from("#main p",{
+    y:40,
+    duration:.5,
+    delay:1.5,
+    opacity:0,
+    stagger:.5
+
+})
+gsap.from(".second img",{
+    y:-40,
+    duration:1,
+    delay:1,
+    opacity:0,
+
+})
